6229a5c62f8e2deb69e5153d91b3de2749fc3651
June Crawley <contact@josephcrawley.dev>
2026-08-22 17:27:11 +0100

    added update frequence variable

--- gameconstants.py
+++ gameconstants.py
@@ -4,6 +4,7 @@ class GAMECONSTANTS:
     GRID_WIDTH = WIDTH // TILE_SIZE
     GRID_HEIGHT = HEIGHT // TILE_SIZE
     FPS= 60
+    update_frequency = FPS*0.5
     Colours = {
         "BLACK" : (0, 0, 0),
         "GREY" : (123, 123, 123),
